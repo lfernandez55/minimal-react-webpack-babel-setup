865c0857444c2ad93a93433093f482f6a2029e4c
lfernandez55 <lfernandez@weber.edu>
2021-04-18 19:49:30 -0600

    Styling dashboard with cards. . . .

--- src/javascripts/components/DashBoard.js
+++ src/javascripts/components/DashBoard.js
@@ -23,8 +23,27 @@ export default function DashBoard() {
                     <Route exact path="/dashboard">
                         <h1>Dashboard</h1>
                         <p>What would you like to do?</p>
-                        <Link to="/admin/users"><button className="btn btn-primary"  >Use Admin Tools</button> </Link>
-                        <Link to="/other"><button className="btn btn-primary"  >Use Other Tools</button> </Link> 
+                        <div className="dashboard-cards">
+                            <Link to="/admin/users">
+                                {/* <button className="btn btn-primary"  >Use Admin Tools</button>  */}
+                                <div className="card custom-card" >
+                                <div className="card-body">
+                                    <h5 className="card-title">Admin Tools</h5>
+                                    <h6 className="card-subtitle mb-2 text-muted">Manage Users and Roles</h6>
+                                </div>
+                                </div>
+                            </Link>
+                            <Link to="/other">
+                                {/* <button className="btn btn-primary"  >Use Other Tools</button>  */}
+                                <div className="card custom-card" >
+                                <div className="card-body">
+                                    <h5 className="card-title">Other Tools</h5>
+                                    <h6 className="card-subtitle mb-2 text-muted">These can be assigned to other roles</h6>
+                                </div>
+                                </div>
+                            </Link> 
+                        </div>
+
                     </Route>
                     <Route path="/admin">
                         <Admin/>
