b8aa282594b62d13d3981bf39105ba20777e33cf
lfernandez55 <lfernandez@weber.edu>
2021-04-22 00:21:25 -0600

    Minor update to createAdmin

--- src/javascripts/controllers/createAdmin.js
+++ src/javascripts/controllers/createAdmin.js
@@ -3,8 +3,10 @@ import {User} from '../models/user'
 
 export const createAdmin = (req, res, next) => {
 
-    const removeDocs = async() => {
+    const seedDB = async() => {
         try {
+
+          // start fresh by removing all documents in the collections
           await User.remove({});
           await Role.remove({});
 
@@ -24,14 +26,9 @@ export const createAdmin = (req, res, next) => {
           res.status(200).json({success: true, message: "Admin account creation successful"})
 
         } catch (err) {
-          return 'error occured';
+            res.status(400).json({success: false, message: err})
         }
     }
 
-    removeDocs()
-
-    
-
-
-
+    seedDB()
 }
