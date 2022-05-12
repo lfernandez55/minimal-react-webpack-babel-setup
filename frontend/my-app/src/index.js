94a3cee1e4c9b5df24b5c2d32d7b0358d25eec42
lfernandez55 <lfernandez@weber.edu>
2022-05-12 16:07:41 -0600

    Installed bootstrap

--- frontend/my-app/src/index.js
+++ frontend/my-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './bootstrap.min.css'
+//import './bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import './main.css';
 import './ReactToastify.min.css'
